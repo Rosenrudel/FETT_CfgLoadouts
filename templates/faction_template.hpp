14c4e8781e28c84b84fd02676fa0f2247dce1e78
Wasserstoff <41219647+GermanHydrogen@users.noreply.github.com>
2023-03-19 15:17:08 +0100

    Added skin option to faction template

--- templates/faction_template.hpp
+++ templates/faction_template.hpp
@@ -1,7 +1,3 @@
-#ifndef PREFIX
-	#define PREFIX
-#endif
-
 #define WITH_PREFIX(clazz) PREFIX##clazz
 
 class WITH_PREFIX(Soldier) : SoldierClass {
@@ -15,8 +11,16 @@ class WITH_PREFIX(Soldier) : SoldierClass {
 		gps_pool[] = {"ItemGPS"};
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : STD_WEAPON {};
-		class handGun : HANDGUN {};
+		class primaryWeapon : STD_WEAPON {
+			#ifdef STD_WEAPON_SKIN
+				skin = STD_WEAPON_SKIN;
+			#endif
+		};
+		class handGun : HANDGUN {
+			#ifdef HANDGUN_SKIN
+				skin = HANDGUN_SKIN;
+			#endif
+		};
 	};
 	class Items : BasicItems {};
 };
@@ -34,7 +38,11 @@ class WITH_PREFIX(Officer) : WITH_PREFIX(Soldier) {
 		gps_pool[] = PDA;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : SMG_WEAPON {};
+		class primaryWeapon : SMG_WEAPON {
+			#ifdef SMG_WEAPON_SKIN
+				skin = SMG_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Items : SmallItems {};
 };
@@ -47,6 +55,10 @@ class WITH_PREFIX(Squadleader) : WITH_PREFIX(Officer) {
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : GL_WEAPON {
+			#ifdef GL_WEAPON_SKIN
+				skin = GL_WEAPON_SKIN;
+			#endif
+
 			magazinesMax = 4;
 			optional[] = {GL_HuntIR_AMMO};
 		};
@@ -85,6 +97,9 @@ class WITH_PREFIX(Rifleman) : WITH_PREFIX(Soldier) {
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
 			optional[] = {
 				{STD_WEAPON_MAG, 4},
 				{AR_WEAPON_MAG, 3}
@@ -98,7 +113,11 @@ class WITH_PREFIX(Rifleman) : WITH_PREFIX(Soldier) {
 
 class WITH_PREFIX(RiflemanAT) : WITH_PREFIX(Rifleman) {
 	class Weapons : Weapons {
-		class secondaryWeapon : LAUNCHER_WEAPON {};
+		class secondaryWeapon : LAUNCHER_WEAPON {
+			#ifdef LAUNCHER_WEAPON_SKIN
+				skin = LAUNCHER_WEAPON_SKIN;
+			#endif
+		};
 	};
 };
 
@@ -107,7 +126,11 @@ class WITH_PREFIX(AutomaticRifleman) : WITH_PREFIX(Soldier) {
 		vest_pool[] = LIGHT_VESTS;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : AR_WEAPON {};
+		class primaryWeapon : AR_WEAPON {
+			#ifdef AR_WEAPON_SKIN
+				skin = AR_WEAPON_SKIN;
+			#endif
+		};
 	};
 };
 
@@ -116,7 +139,11 @@ class WITH_PREFIX(HeavyGunner) : WITH_PREFIX(AutomaticRifleman) {
 		vest_pool[] = LIGHT_VESTS;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : MMG_WEAPON {};
+		class primaryWeapon : MMG_WEAPON {
+			#ifdef MMG_WEAPON_SKIN
+				skin = MMG_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Items : SmallItems {};
 };
@@ -126,7 +153,11 @@ class WITH_PREFIX(Grenadier) : WITH_PREFIX(Soldier) {
 		vest_pool[] = GL_VESTS;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : GL_WEAPON {};
+		class primaryWeapon : GL_WEAPON {
+			#ifdef GL_WEAPON_SKIN
+				skin = GL_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Items : SmallItems {};
 };
@@ -136,23 +167,35 @@ class WITH_PREFIX(DesignatedMarksman) : WITH_PREFIX(Soldier) {
 		vest_pool[] = LIGHT_VESTS;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : DMR_WEAPON {};
+		class primaryWeapon : DMR_WEAPON {
+			#ifdef DMR_WEAPON_SKIN
+				skin = DMR_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Gear : Gear {
 		binocular_pool[] = {"Binocular"};
 	};
 };
 
-class WITH_PREFIX(ATSpecialist) : WITH_PREFIX(Soldier) {
+class WITH_PREFIX(SpecialistAT) : WITH_PREFIX(Soldier) {
 	class Container : Container {
 		vest_pool[] = LIGHT_VESTS;
 		backpack_pool[] = SMALL_BACKPACKS;
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
+
 			magazinesMax = 4;
 		};
-		class secondaryWeapon : LAUNCHER_HEAVY_WEAPON {};
+		class secondaryWeapon : LAUNCHER_HEAVY_WEAPON {
+			#ifdef LAUNCHER_HEAVY_WEAPON_SKIN
+				skin = LAUNCHER_HEAVY_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Items : SmallItems {};
 };
@@ -161,7 +204,17 @@ class WITH_PREFIX(CombatMedic) : WITH_PREFIX(Rifleman) {
 	class Container : Container {
 		vest_pool[] = HEAVY_VESTS;
 	};
-	class Gear : BasicGear {};
+	class Weapons : Weapons {
+		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
+			optional[] = {};
+		};
+	};
+	class Gear : BasicGear {
+		nightvision_pool[] = NV;
+	};
 	class Items : CombatMedicItems {};
 
 	onApplyLoadout = "_this setVariable ['ace_medical_medicclass', 1, true];";
@@ -169,7 +222,11 @@ class WITH_PREFIX(CombatMedic) : WITH_PREFIX(Rifleman) {
 
 class WITH_PREFIX(Medic): WITH_PREFIX(CombatMedic) {
 	class Weapons : Weapons {
-		class primaryWeapon : SMALL_WEAPON {};
+		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Container : Container {
 		vest_pool[] = SMALL_VESTS;
@@ -195,6 +252,9 @@ class WITH_PREFIX(AmmoBearerMMG) : WITH_PREFIX(Soldier) {
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
 			magazinesMax = 4;
 			optional[] = {
 				{MMG_WEAPON_MAG, 3}
@@ -231,6 +291,10 @@ class WITH_PREFIX(Commander) : WITH_PREFIX(Soldier) {
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : SMALL_WEAPON {
+			#ifdef SMALL_WEAPON_SKIN
+				skin = SMALL_WEAPON_SKIN;
+			#endif
+
 			magazinesMax = 2;
 		};
 		class handGun : HANDGUN {};
@@ -239,7 +303,9 @@ class WITH_PREFIX(Commander) : WITH_PREFIX(Soldier) {
 };
 
 class WITH_PREFIX(Crew) : WITH_PREFIX(Commander) {
-	class Gear : BasicGear {};
+	class Gear : BasicGear {
+		nightvision_pool[] = NV;
+	};
 };
 
 class WITH_PREFIX(HeliPilot) : WITH_PREFIX(Commander) {
@@ -249,7 +315,11 @@ class WITH_PREFIX(HeliPilot) : WITH_PREFIX(Commander) {
 		vest_pool[] = TAC_VEST;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : SMG_WEAPON {};
+		class primaryWeapon : SMG_WEAPON {
+			#ifdef SMG_WEAPON_SKIN
+				skin = SMG_WEAPON_SKIN;
+			#endif
+		};
 	};
 };
 
@@ -259,7 +329,9 @@ class WITH_PREFIX(HeliCrew) : WITH_PREFIX(HeliPilot) {
 		uniform_pool[] = HELI_CREW_UNIFORMS;
 		vest_pool[] = TAC_VEST;
 	};
-	class Gear : BasicGear {};
+	class Gear : BasicGear {
+		nightvision_pool[] = NV;
+	};
 };
 
 class WITH_PREFIX(Sniper) : WITH_PREFIX(Soldier) {
@@ -268,14 +340,22 @@ class WITH_PREFIX(Sniper) : WITH_PREFIX(Soldier) {
 		vest_pool[] = CARRIER_VESTS;
 	};
 	class Weapons : Weapons {
-		class primaryWeapon : SNIPER_WEAPON {};
+		class primaryWeapon : SNIPER_WEAPON {
+			#ifdef SNIPER_WEAPON_SKIN
+				skin = SNIPER_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Items : SpecialGrenadesSmall {};
 };
 
 class WITH_PREFIX(Spotter) : WITH_PREFIX(Sniper) {
 	class Weapons : Weapons {
-		class primaryWeapon : DMR_WEAPON {};
+		class primaryWeapon : DMR_WEAPON {
+			#ifdef DMR_WEAPON_SKIN
+				skin = DMR_WEAPON_SKIN;
+			#endif
+		};
 	};
 	class Gear : Gear {
 		radios[] = {"ACRE_PRC343","ACRE_PRC148"};
@@ -299,6 +379,9 @@ class WITH_PREFIX(Logistician) : WITH_PREFIX(Soldier) {
 	};
 	class Weapons : Weapons {
 		class primaryWeapon : SMG_WEAPON {
+			#ifdef SMG_WEAPON_SKIN
+				skin = SMG_WEAPON_SKIN;
+			#endif
 			magazinesMax = 2;
 		};
 		class handGun : handGun {
@@ -323,6 +406,9 @@ class WITH_PREFIX(LogisticianTeamleader) : WITH_PREFIX(Logistician) {
 class WITH_PREFIX(Artillery) : WITH_PREFIX(Soldier) {
 	class Weapons : Weapons {
 		class primaryWeapon : SMG_WEAPON {
+			#ifdef SMG_WEAPON_SKIN
+				skin = SMG_WEAPON_SKIN;
+			#endif
 			magazinesMax = 2;
 		};
 		class handGun : handGun {
